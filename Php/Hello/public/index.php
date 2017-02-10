9ce8be9c63718ccd9236f87846dc7cf840e7749e
rcerqueira11 <raxs64@gmail.com>
2017-02-10 09:40:26 -0400

    Php course

---
+++ Php/Hello/public/index.php
@@ -0,0 +1,17 @@
+<?php
+	require __DIR__ . '/../app/src/app.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Demo</title>
+</head>
+<body>
+<?php
+	include('../app/views/header.php');
+	include('../app/views/content.php');
+	include('../app/views/footer.php');
+?>
+</body>
+</html>
