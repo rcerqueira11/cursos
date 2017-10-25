362c10b33bec3eb074c12eea0664f2823eecec52
Ricardo Cerqueira <raxs64@gmail.com>
2017-10-25 11:46:38 -0400

    endo tuto

--- Loopsback/loopback-getting-started-II/common/models/review.js
+++ Loopsback/loopback-getting-started-II/common/models/review.js
@@ -1,10 +1,10 @@
 'use strict';
 
 module.exports = function(Review) {
-    // Review.beforeRemote('create', function(context, user, next){
-    //     context.args.data.date = Date.now();
-    //     context.args.data.publisherId = context.req.accessToken.userId;
-    //     next();
-    // });
+    Review.beforeRemote('create', function(context, user, next){
+        context.args.data.date = Date.now();
+        context.args.data.publisherId = context.req.accessToken.userId;
+        next();
+    });
 
 };
