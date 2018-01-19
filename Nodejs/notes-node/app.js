4282c14101bf38c7b61a347ea28b6e5e845ec0f1
rcerqueira11 <raxs64@gmail.com>
2018-01-19 10:06:50 -0400

    Node

--- Nodejs/notes-node/app.js
+++ Nodejs/notes-node/app.js
@@ -2,6 +2,24 @@ console.log('Starting app');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
+// console.log(process.argv);
+const argv = yargs.argv;
+var command = argv._[0];
+
+// console.log('command', command);
+
+if (command === 'add'){
+    notes.addNote(argv.title, argv.body);
+}else if (command === 'list'){
+    notes.getAll();
+}else if (command === 'read'){
+    notes.readingNote(argv.title);
+}else if (command === 'remove'){
+    notes.removeNote(argv.title);
+}else {
+    console.log("Command not recognized ");
+}
--- Nodejs/notes-node/notes.js
+++ Nodejs/notes-node/notes.js
@@ -6,7 +6,32 @@ console.log('Start notes.js');
 // 	return num1 + num2;
 // };
 
-module.exports.addNote = () => {
-	console.log('add');
-	return 'New note';
+// module.exports.addNote = () => {
+// 	console.log('add');
+// 	return 'New note';
+// };
+
+var addNote = (title, body) => {
+	console.log(`adding ${title} ${body} `)
 };
+
+var getAll = () => {
+	console.log("Getting all notes")
+};
+
+var removeNote = (title) => {
+	console.log(`Removing Note ${title} `)
+};
+
+var readingNote = (title) => {
+	console.log(`Reading note ${title} `);
+};
+
+
+module.exports = {
+	// addNote: addNote si son iguales se puede poner 
+	addNote,
+	getAll,
+	removeNote,
+	readingNote
+}
