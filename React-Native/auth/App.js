1f8ecc21f75dff9678feb1e17f6b64ac7b6a7302
rcerqueira11CF <raxs64@gmail.com>
2018-08-28 10:01:56 -0300

    remove api key

--- React-Native/auth/App.js
+++ React-Native/auth/App.js
@@ -4,19 +4,12 @@ import axios from 'axios';
 import firebase from 'firebase';
 import { Header } from './src/components/common';
 import LoginForm from './src/components/LoginForm';
-
+import { firebaseApiKey } from './apiKeys/apikeys'
 export default class App extends React.Component {
 
 
   componentWillMount() {
-    firebase.initializeApp({
-      apiKey: 'AIzaSyCok203zernl4E1Hg-lSE7RLzBUfc9MvBg',
-      authDomain: 'auth-70464.firebaseapp.com',
-      databaseURL: 'https://auth-70464.firebaseio.com',
-      projectId: 'auth-70464',
-      storageBucket: 'auth-70464.appspot.com',
-      messagingSenderId: '1017296346248'
-    })
+    firebase.initializeApp(firebaseApiKey)
   }
 
 
