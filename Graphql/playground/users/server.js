99bfdeb8b4a574d38d89c33a2cc3b16dcc09692f
Ricardo Cerqueira <raxs64@gmail.com>
2019-08-23 19:36:22 -0400

    [GRAPHQL] graphiql

--- Graphql/playground/users/server.js
+++ Graphql/playground/users/server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql');
 
 const app = express();
+
+// tell the app that anything that comes with /graphql should be handled by graphql
+
+app.use('/graphql', expressGraphQL({
+  graphiql: true //allows us to make request to development server, only for dev
+}));
 
 app.listen(4000,()=>{
   console.log('Listening');
 });
+
+// go to http://localhost:4000/graphql
