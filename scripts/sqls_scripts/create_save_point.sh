f94efa74f76f4c1a74a2537b77149129eac25864
Ricardo Cerqueira <raxs64@gmail.com>
2019-08-30 13:43:35 -0400

    update

--- scripts/sqls_scripts/create_save_point.sh
+++ scripts/sqls_scripts/create_save_point.sh
@@ -5,5 +5,5 @@ sqls_path="/home/rcerqueira/Proyectos/ComunidadFeliz/scripts/sqls/"
 [ -d $sqls_path ] || mkdir $sqls_path
 
 echo "Bajando dump de $nombre_bd"
-PGPASSWORD='comunidad-feliz' pg_dump -h localhost -p 5432 -U comunidadfeliz "$nombre_bd" > $sqls_path"dump_"$nombre_bd"_"$fecha".sql
+PGPASSWORD='comunidad-feliz' pg_dump -h localhost -p 5432 -U comunidadfeliz "$nombre_bd" > $sqls_path'dump_'$nombre_bd'_'$fecha'.sql'
 
