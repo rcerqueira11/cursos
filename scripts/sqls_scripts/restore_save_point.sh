736b0bb4c51d44a78db9418bce57ea30638e15a9
Ricardo Cerqueira <raxs64@gmail.com>
2019-08-30 15:37:25 -0400

    fix bad defines

--- scripts/sqls_scripts/restore_save_point.sh
+++ scripts/sqls_scripts/restore_save_point.sh
@@ -3,7 +3,7 @@ echo "comunidadfeliz_development"
 nombre_bd="comunidadfeliz_development"
 sqls_path="/home/rcerqueira/Proyectos/ComunidadFeliz/scripts/sqls/"
 PARAMETER=$1
-NUMB=${PARAMETER-1}
+NUMB=${PARAMETER:-1}
 nombre_sql=$(ls $sqls_path -t | head -n$NUMB | tail -n 1)
 sql_path_name="$sqls_path$nombre_sql"
 echo $sql_path_name
