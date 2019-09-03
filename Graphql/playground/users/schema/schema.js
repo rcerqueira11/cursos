158672d4a31ed9d42be0303eaf15b74211561689
Ricardo Cerqueira <raxs64@gmail.com>
2019-09-03 01:04:48 -0400

    [GRAPHQL] Adv graphql

---
+++ Graphql/playground/users/schema/schema.js
@@ -0,0 +1,42 @@
+// All the knowladge
+const graphql = require('graphql');
+const _ = require('lodash');
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema
+} = graphql;
+
+const users = [
+  { id: '23', firstName: 'Bill', age: 20},
+  { id: '47', firstName: 'Samantha', age: 21}
+];
+
+const UserType = new GraphQLObjectType({
+  name: 'User', //describe type we are defining
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  }
+});
+
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      //args is present in args
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+})
--- Graphql/playground/users/server.js
+++ Graphql/playground/users/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
 // tell the app that anything that comes with /graphql should be handled by graphql
 
 app.use('/graphql', expressGraphQL({
+  schema,
   graphiql: true //allows us to make request to development server, only for dev
 }));
 
