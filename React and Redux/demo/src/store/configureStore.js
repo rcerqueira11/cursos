1ba4a20b098e6fbce185e128629da228a81ea8ce
Ricardo Cerqueira <raxs64@gmail.com>
2019-09-11 12:08:29 -0300

    fix configureStore

--- React and Redux/demo/src/store/configureStore.js
+++ React and Redux/demo/src/store/configureStore.js
@@ -1,7 +1,7 @@
-//Dynamic import arent supporte by ES6, 
+//Dynamic import arent supporte by ES6,
 // so using require instead of import
 if (process.env.NODE_ENV === 'production'){
     module.exports = require('./configureStore.prod');
 } else {
-    module.export = require('./configureStore.dev');
+    module.exports = require('./configureStore.dev');
 }
